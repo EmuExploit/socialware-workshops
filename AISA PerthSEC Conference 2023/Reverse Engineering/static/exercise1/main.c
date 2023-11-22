516caa698cc5d64829805e4a31d698a052bd7ec8
torry2 <github@torrytw.ooo>
2023-11-22 22:18:55 +0800

    commit

---
+++ AISA PerthSEC Conference 2023/Reverse Engineering/static/exercise1/main.c
@@ -0,0 +1,23 @@
+// FIX ME!!
+
+
+int fibonacci(unsigned int n) {
+	int prev = 0;
+	int curr = 1;
+	int tmp  = 0;
+
+	for (int i = 0; i < n; i ++) {
+		tmp = curr // FIX ME!!
+		curr = curr + prev;
+		prev = tmp;
+
+		printf("", curr); // FIX ME!!
+	}
+
+	return curr;
+}
+
+int main(void) {
+	// FIX ME!!
+	return 0;
+}
---
+++ AISA PerthSEC Conference 2023/Reverse Engineering/static/exercise2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void encrypt(char* s, /* FIX ME*/) {
+	for (int i = 0; /*FIX ME!! */; i ++) {
+		s[i] += key;
+	}
+}
+
+
+int main(void) {
+	char plaintext[] = "EMUEXPLOIT";
+	
+	encrypt(plaintext,13);
+	//FIX ME!!
+}
